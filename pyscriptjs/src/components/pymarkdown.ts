2c246fad5282cd855d77409a53b15812c9ace66e
Fabio Pliger <fabio.pliger@gmail.com>
2022-05-22 16:02:57 -0500

    add pymarkdown

---
+++ pyscriptjs/src/components/pymarkdown.ts
@@ -0,0 +1,31 @@
+import { BaseEvalElement } from './base';
+import { addClasses, htmlDecode } from '../utils';
+import { marked } from 'marked';
+
+export class PyMarkdown extends BaseEvalElement {
+    shadow: ShadowRoot;
+    wrapper: HTMLElement;
+    theme: string;
+    widths: Array<string>;
+    src: string;
+    mount_name: string;
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.checkId();
+        this.src = htmlDecode(this.innerHTML);
+        this.mount_name = this.id.split('-').join('_');
+        this.innerHTML = '';
+
+        const mainDiv = document.createElement('div');
+
+        mainDiv.id = this.id;
+        this.id = `${this.id}-container`;
+        console.log("We are doing this...")
+        mainDiv.innerHTML = marked.parse(this.src)
+        console.log(marked.parse(this.src))
+        this.appendChild(mainDiv);
+    }
+}
